089bbec9ea98672f6286f7b73391725610248f67
Alexis <AlexDdevs@gmail.com>
2024-05-15 19:55:31 +0200

    Create the routing and add page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react"
+import { Routes, Route, useLocation } from "react-router-dom"
+import Header from "./components/header/Header"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
 
 function App() {
+    const location = useLocation().pathname;
+
     return (
-        <div>CONTENT</div>
+        <>
+            <Header />
+            <Routes location={location} key={location} >
+                <Route index element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </>
     )
 }
 
