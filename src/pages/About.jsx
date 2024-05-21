88b6b4c6c25e974bec5010f6354f296fd1529c17
Alexis <AlexDdevs@gmail.com>
2024-05-21 14:36:39 +0200

    Create a component for About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import moi from "../assets/img/portrait.webp"
 
 export default function About() {
-  return (
-    <div>About</div>
-  )
+    return (
+        <main className="main-about">
+            <div className="about-container">
+                <section className="about-me">
+                    <h1 className="hello">Hi there!</h1>
+                    <h2 className='name'>I'm Alexis</h2>
+                    <p className="description">
+                        <strong>Web developer</strong> looking for companies. After a year of self-taught training alongside my job (pharmacy assistant), I took a 9-month web integrator course with OpenClassroom. An apprenticeship would give me the opportunity to gain professional experience, discover teamwork, develop my collaborative skills, perfect everything I've learnt and learn new technologies, so that I can grow in this exciting field that I'm passionate about.
+                    </p>
+                </section>
+                <div className="cv-container">
+                    <a href="https://delporte-alexis-cv.vercel.app/" target='_blank' title='Mon CV' className='cv-link'>
+                        <i className="fa-solid fa-file"></i>
+                        Mon CV
+                    </a>
+                </div>
+            </div>
+            <div className="avatar">
+                <img src={moi} alt="Image d'Alexis Delporte" />
+            </div>
+        </main>
+    )
 }
