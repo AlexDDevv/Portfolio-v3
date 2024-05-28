20a8d6f934cfdc824faca7ac31a92ebf0af405f1
Alexis <AlexDdevs@gmail.com>
2024-05-28 16:10:11 +0200

    Add responsive and put email btn and span in a div

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -29,12 +29,14 @@ const Contact = () => {
             <section className="contact-me">
                 <h1>Me contacter</h1>
                 <p><strong>À l'écoute</strong> de toutes les opportunités qui peuvent se présenter à moi, alors si mon profil vous semble intéressant, vous pouvez toujours me contacter par mes différents réseaux sociaux ou par email!</p>
-                <button className='btn-copy' onClick={copyEmail}>
-                    <span className="email">alexddevs@gmail.com</span>
-                    <span className="fa-regular fa-copy" aria-hidden="true"></span>
-                    <span className="sr-only">Copier l'adresse email</span>
-                </button>
-                <span className="copy-msg">Copiée?</span>
+                <div className="email-container">
+                    <button className='btn-copy' onClick={copyEmail}>
+                        <span className="email">alexddevs@gmail.com</span>
+                        <span className="fa-regular fa-copy" aria-hidden="true"></span>
+                        <span className="sr-only">Copier l'adresse email</span>
+                    </button>
+                    <span className="copy-msg">Copiée?</span>
+                </div>
             </section>
             <div className="contact-img">
                 <img src={desk} alt="Un bureau avec un ordinateur portable, un café et de quoi écrire." />
