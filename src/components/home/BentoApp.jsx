c4b741ea67418e207d2c41bfc45ba701e6804fd2
Alexis <AlexDdevs@gmail.com>
2024-06-18 13:52:17 +0200

    Modif for a better integration

--- src/components/home/BentoApp.jsx
+++ src/components/home/BentoApp.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom"
 
 export default function ({ name, title, subtitle, cover }) {
     return (
-        <div className={`bento-item ${name}`}>
+        <Link to={"/projects"} className={`bento-item ${name}`}>
             <div className="titles">
                 <h2 className="title">{title}</h2>
                 <h3 className="subtitle">{subtitle}</h3>
             </div>
             <div className="cover">
-                <img src={cover} alt={`Application ${title}`} />
+                <img 
+                    src={cover} 
+                    alt={`Application ${title}`} 
+                    loading='lazy'
+                />
             </div>
-            <Link to={"/projects"} className='redirection'>
-                <span className="fa-solid fa-square-arrow-up-right" aria-hidden="true"></span>
-                <span className="sr-only">Redirection vers la page Projets</span>
-            </Link>
-        </div>
+        </Link>
     )
 }
